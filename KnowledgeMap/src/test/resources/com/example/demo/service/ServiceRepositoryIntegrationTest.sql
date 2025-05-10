0556e04ba54d7cc6dbafaca7944eb97a75519015
Yamada <miosamekosame@yahoo.ne.jp>
2025-05-10 17:01:32 +0900

    CategoryServiceの単体テスト

--- KnowledgeMap/src/test/resources/com/example/demo/service/ServiceRepositoryIntegrationTest.sql
+++ KnowledgeMap/src/test/resources/com/example/demo/service/ServiceRepositoryIntegrationTest.sql
@@ -1,3 +1,6 @@
-INSERT INTO category(name) VALUES ('category1');
-INSERT INTO category(name) VALUES ('category2');
-INSERT INTO word(word_name, content, category_id) VALUES('word1', 'content1', 1);
+
+INSERT INTO users(id, username, password) VALUES(1, 'testuser', 'pass');
+INSERT INTO wordbook(id, name, user_id) VALUES(1, 'testWordbook', 1);
+
+INSERT INTO category(name, wordbook_id) VALUES('testCategory', 1);
+INSERT INTO word(word_name, content, category_id, wordbook_id) VALUES('word1', 'content1', 1, 1);
